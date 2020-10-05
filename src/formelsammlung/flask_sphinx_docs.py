d4c8022c4a957d2cb58cd90033c3a66d8729ece3
Cielquan <cielquan@protonmail.com>
2020-10-05 17:25:14 +0200

    added SphinxDocServer; closes #4

---
+++ src/formelsammlung/flask_sphinx_docs.py
@@ -0,0 +1,46 @@
+# noqa: D205,D208,D400
+"""
+    formelsammlung.flask_sphinx_docs
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Serve sphinx docs in your flask app.
+
+    :copyright: (c) Christian Riedel
+    :license: GPLv3
+"""
+from typing import Optional
+
+from flask import Flask
+
+
+class SphinxDocServer:
+    """Serve your sphinx docs under `/docs/` on your own flask app."""
+
+    def __init__(self, app: Optional[Flask] = None, **kwargs) -> None:
+        """Init SphinxDocServer."""
+        if app is not None:
+            self.init_app(app, **kwargs)
+
+    def init_app(
+        self, app: Flask, doc_dir: str, index_file: str = "index.html"
+    ) -> None:
+        """Add the `/docs/` route to the `app` object.
+
+        :param app: Flask object to add the route to.
+        :param doc_dir: The base directory holding the sphinx docs to serve.
+        :param index_file: The html file containing the base toctree.
+            Default: "index.html"
+        """
+
+        @app.route("/docs/", defaults={"filename": index_file})
+        @app.route("/docs/<path:filename>")
+        def web_docs(filename):
+            """Route the given doc page.
+
+            :param filename: File name from URL
+            :return: Requested doc page
+            """
+            app.static_folder = doc_dir
+            doc_file = app.send_static_file(filename)
+            app.static_folder = "static"
+            return doc_file
