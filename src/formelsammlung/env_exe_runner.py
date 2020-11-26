507f995a0f8a8289915ffd129abc83f6de0dad2e
Cielquan <cielquan@protonmail.com>
2020-11-26 11:31:27 +0100

    env_exe_runner noow takes a list of runner

--- src/formelsammlung/env_exe_runner.py
+++ src/formelsammlung/env_exe_runner.py
@@ -14,21 +14,21 @@ from pathlib import Path
 from typing import List, Optional
 
 
-def env_exe_runner(
-    runner: str, envs: List[str], tool: str, tool_args: Optional[List[str]] = None
+def env_exe_runner(  # pylint: disable=R0912
+    venv_runner: List[str],
+    envs: List[str],
+    tool: str,
+    tool_args: Optional[List[str]] = None,
 ) -> int:
-    """Call given ``tool`` from given ``tox`` or ``nox`` env considering OS.
+    """Call given ``tool`` from given `tox` or `nox` or `virtual` env considering OS.
 
-    :param runner: 'nox' or 'tox'
-    :param envs: List of environments to search the ``tool`` in.
+    :param venv_runner: 'nox' and/or 'tox' and/or a 'virtual environment'
+    :param envs: List of environments to search the ``tool`` in when 'tox' or 'nox' is
+        in venv_runner.
     :param tool: Name of the executable to run.
     :param tool_args: Arguments to give to the ``tool``.
     :return: Exit code 127 if no executable is found or the exit code of the called cmd.
     """
-    if runner not in ("tox", "nox"):
-        print("No valid runner was given. Choose either 'tox' or 'nox'.")
-        return 127
-
     is_win = sys.platform == "win32"
 
     exe = Path(f"Scripts/{tool}.exe") if is_win else Path(f"bin/{tool}")
@@ -37,16 +37,29 @@ def env_exe_runner(
     if not tool_args:
         tool_args = []
 
-    for env in envs:
-        path = Path(f".{runner}") / env / exe
-        if path.is_file():
-            cmd = (str(path), *tool_args)
+    for runner in venv_runner:
+        if runner in ("tox", "nox"):
+            for env in envs:
+                path = Path(f".{runner}") / env / exe
+                if path.is_file():
+                    cmd = (str(path), *tool_args)
+                    break
+        else:
+            path = Path(runner) / exe
+            if path.is_file():
+                cmd = (str(path), *tool_args)
+        if cmd:
+            break
 
     if cmd is None:
-        print(
-            f"No '{tool}' executable found. Make sure one of the "
-            f"following `{runner}` envs is accessible: {envs}"
-        )
+        print(f"No '{tool}' executable found. Search in:")
+        for runner in venv_runner:
+            if runner == "tox":
+                print(f"- 'tox' envs: {envs}")
+            elif runner == "nox":
+                print(f"- 'nox' envs: {envs}")
+            else:
+                print(f"- virtual env: ['{runner}']")
         return 127
 
     return subprocess.call(cmd)  # nosec
@@ -69,7 +82,7 @@ def cli_caller() -> int:
     :return: Exit code from ``env_exe_runner``
     """
     return env_exe_runner(
-        sys.argv[1], sys.argv[2].split(","), sys.argv[3], sys.argv[4:]
+        sys.argv[1].split(","), sys.argv[2].split(","), sys.argv[3], sys.argv[4:]
     )  # pragma: no cover
 
 
