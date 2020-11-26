b05f0067a4bfbfc9e3ef92ccc992803526272eca
Cielquan <cielquan@protonmail.com>
2020-11-26 11:53:39 +0100

    updated test_env_exe_runner

--- tests/test_env_exe_runner.py
+++ tests/test_env_exe_runner.py
@@ -15,27 +15,40 @@ from pathlib import Path
 from formelsammlung.env_exe_runner import env_exe_runner as runner
 
 
-def test_without_args(monkeypatch):
+def test_runner_without_args(monkeypatch):
     """Test runner without additional arguments."""
     monkeypatch.setattr(subprocess, "call", lambda _: 32)
     monkeypatch.setattr(Path, "is_file", lambda _: True)
-    assert runner("tox", ["toxenv"], "tool") == 32
+    assert runner(["tox"], ["toxenv"], "tool") == 32
 
 
-def test_with_args(monkeypatch):
+def test_runner_with_args(monkeypatch):
     """Test runner with additional arguments."""
     monkeypatch.setattr(subprocess, "call", lambda _: 32)
     monkeypatch.setattr(Path, "is_file", lambda _: True)
-    assert runner("tox", ["toxenv"], "tool", ["argument"]) == 32
+    assert runner(["tox"], ["toxenv"], "tool", ["argument"]) == 32
+
+
+def test_venv_without_args(monkeypatch):
+    """Test venv without additional arguments."""
+    monkeypatch.setattr(subprocess, "call", lambda _: 32)
+    monkeypatch.setattr(Path, "is_file", lambda _: True)
+    assert runner(["venv"], [], "tool") == 32
+
+
+def test_venv_with_args(monkeypatch):
+    """Test venv with additional arguments."""
+    monkeypatch.setattr(subprocess, "call", lambda _: 32)
+    monkeypatch.setattr(Path, "is_file", lambda _: True)
+    assert runner(["venv"], [], "tool", ["argument"]) == 32
 
 
 def test_no_cmd_found(capsys):
     """Test exit code 127 on missing cmd."""
-    assert runner("tox", ["non_existing_toxenv"], "no_existing_tool") == 127
-    assert "No 'no_existing_tool' executable found." in capsys.readouterr().out
+    result = runner(["tox", "venv"], ["non_existing_toxenv"], "no_existing_tool")
+    output = capsys.readouterr().out
 
-
-def test_invalid_runner(capsys):
-    """Test exit code 127 on invalid runner."""
-    assert runner("not_tox_nor_nox", ["non_existing_toxenv"], "no_existing_tool") == 127
-    assert "No valid runner was given." in capsys.readouterr().out
+    assert result == 127
+    assert "No 'no_existing_tool' executable found." in output
+    assert "- 'tox' envs: ['non_existing_toxenv']" in output
+    assert "- virtual env: ['venv']" in output
