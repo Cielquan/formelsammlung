23a1e5134f9b50c4e0f274b2a38718c62495b2b7
Christian Riedel <cielquan@protonmail.com>
2021-02-05 18:15:56 +0100

    add unit tests for refactored functions
    change import of tested module

--- tests/test_env_exe_runner.py
+++ tests/test_env_exe_runner.py
@@ -7,13 +7,25 @@
     :copyright: (c) 2020, Christian Riedel and AUTHORS
     :license: GPL-3.0-or-later, see LICENSE for details
 """  # noqa: D205,D208,D400
+import os
 import subprocess  # noqa: S404
 
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Generator
 
 import pytest
 
-from formelsammlung.env_exe_runner import env_exe_runner as runner
+from formelsammlung import env_exe_runner
+
+
+@contextmanager
+def change_cwd(target: Path) -> Generator:
+    """Change cwd with a contextmanager."""
+    cwd = os.getcwd()
+    os.chdir(target)
+    yield
+    os.chdir(cwd)
 
 
 def test_runner_without_args(monkeypatch: pytest.MonkeyPatch) -> None:
@@ -21,7 +33,7 @@ def test_runner_without_args(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr(subprocess, "call", lambda _: 32)
     monkeypatch.setattr(Path, "is_file", lambda _: True)
 
-    result = runner(["tox"], ["toxenv"], "tool")
+    result = env_exe_runner.env_exe_runner(["tox"], ["toxenv"], "tool")
 
     assert result == 32
 
@@ -31,7 +43,7 @@ def test_runner_with_args(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr(subprocess, "call", lambda _: 32)
     monkeypatch.setattr(Path, "is_file", lambda _: True)
 
-    result = runner(["tox"], ["toxenv"], "tool", ["argument"])
+    result = env_exe_runner.env_exe_runner(["tox"], ["toxenv"], "tool", ["argument"])
 
     assert result == 32
 
@@ -41,7 +53,7 @@ def test_venv_without_args(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr(subprocess, "call", lambda _: 32)
     monkeypatch.setattr(Path, "is_file", lambda _: True)
 
-    result = runner(["venv"], [], "tool")
+    result = env_exe_runner.env_exe_runner(["venv"], [], "tool")
 
     assert result == 32
 
@@ -51,17 +63,100 @@ def test_venv_with_args(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr(subprocess, "call", lambda _: 32)
     monkeypatch.setattr(Path, "is_file", lambda _: True)
 
-    result = runner(["venv"], [], "tool", ["argument"])
+    result = env_exe_runner.env_exe_runner(["venv"], [], "tool", ["argument"])
 
     assert result == 32
 
 
 def test_no_cmd_found(capsys: pytest.CaptureFixture) -> None:
     """Test exit code 127 on missing cmd."""
-    result = runner(["tox", "venv"], ["non_existing_toxenv"], "no_existing_tool")
+    result = env_exe_runner.env_exe_runner(
+        ["tox", "venv"], ["non_existing_toxenv"], "no_existing_tool"
+    )
 
     assert result == 127
     output = capsys.readouterr().out
     assert "No 'no_existing_tool' executable found." in output
     assert "- 'tox' envs: ['non_existing_toxenv']" in output
     assert "- virtual env: ['venv']" in output
+
+
+def test__check_venv_success(tmp_path: Path) -> None:
+    """Test return value of _check_venv for success."""
+    venv_name = ".venv"
+    venv = tmp_path / venv_name
+    venv.mkdir()
+    filename = "testfile"
+    exe = venv / env_exe_runner.EXE.format(tool=filename)
+    exe.parent.mkdir()
+    exe.write_text("This is a file.")
+    #: Change cwd to tmp dir
+    with change_cwd(tmp_path):
+
+        result = env_exe_runner._check_venv(venv_name, filename)
+
+        assert result is not None
+        assert result.absolute() == Path(exe)
+
+
+def test__check_venv_fail(tmp_path: Path) -> None:
+    """Test return value of _check_venv for failure."""
+    venv_name = ".venv"
+    venv = tmp_path / venv_name
+    venv.mkdir()
+    filename = "testfile"
+    #: Change cwd to tmp dir
+    with change_cwd(tmp_path):
+
+        result = env_exe_runner._check_venv(venv_name, filename)
+
+        assert result is None
+
+
+@pytest.mark.parametrize("runner_name", ["tox", "nox"])
+def test__check_runner_envs_success(runner_name: str, tmp_path: Path) -> None:
+    """Test return value of _check_runner_envs for success."""
+    #: Create runner dir
+    runner_dir = tmp_path / ("." + runner_name)
+    runner_dir.mkdir()
+    #: Create env dirs
+    env1_dir = runner_dir / "env1"
+    env1_dir.mkdir()
+    env2_dir = runner_dir / "env2"
+    env2_dir.mkdir()
+    #: Create testfile in second env
+    filename = "testfile"
+    exe = env2_dir / env_exe_runner.EXE.format(tool=filename)
+    exe.parent.mkdir()
+    exe.write_text("This is a file.")
+    #: Change cwd to tmp dir
+    with change_cwd(tmp_path):
+
+        result = env_exe_runner._check_runner_envs(
+            runner_name, ["env1", "env2"], filename
+        )
+
+        assert result is not None
+        assert result.absolute() == Path(exe)
+
+
+@pytest.mark.parametrize("runner_name", ["tox", "nox"])
+def test__check_runner_envs_fail(runner_name: str, tmp_path: Path) -> None:
+    """Test return value of _check_runner_envs for failure."""
+    #: Create runner dir
+    runner_dir = tmp_path / ("." + runner_name)
+    runner_dir.mkdir()
+    #: Create env dirs
+    env1_dir = runner_dir / "env1"
+    env1_dir.mkdir()
+    env2_dir = runner_dir / "env2"
+    env2_dir.mkdir()
+    filename = "testfile"
+    #: Change cwd to tmp dir
+    with change_cwd(tmp_path):
+
+        result = env_exe_runner._check_runner_envs(
+            runner_name, ["env1", "env2"], filename
+        )
+
+        assert result is None
